4cf4835c543ca46c71af236c041b9f66b05cd666
Phan Cong Long <pcl2k2@gmail.com>
2021-08-10 21:23:58 +0700

    Add Content-Prevent-Policy

--- controller/viewController.js
+++ controller/viewController.js
@@ -22,16 +22,20 @@ exports.getTour = catchAsync(async (req, res, next) => {
   if (!tour) {
     return next(new AppError('There is no tour with that name', 404));
   }
-  res
-    .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
-    .render('tour', {
-      title: `${tour.title} Tour`,
-      tour,
-    });
+  // res
+  //   .status(200)
+  //   .set(
+  //     'Content-Security-Policy',
+  //     "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+  //   )
+  //   .render('tour', {
+  //     title: `${tour.title} Tour`,
+  //     tour,
+  //   });
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
 });
 
 exports.getLoginForm = (req, res) => {
